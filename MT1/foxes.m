6625766194638239afc6005990243574faf2562d
Nathan Garvey <garvey.na@gmail.com>
2013-05-09 23:24:47 -0700

    update to preditor-prey model

--- MT1/foxes.m
+++ MT1/foxes.m
@@ -1,3 +1,6 @@
+%program solves and plots the Lotka-Volterra preditor-prey
+%system of equations
+%
 %dx/dt = ax(t) - cy(t)x(t)
 %dy/dt = -by(t) + dx(t)y(t)
 %x(t) = rabits
@@ -6,25 +9,26 @@
 %b = fox death rate
 %c,d characterize animal interactions
 
-clear all;
 
-a = 2*12; %[1/mo]
-b = 1*12; %[1/mo]
-c = 0.12; %[1/year]
-d = 0.06; %[1/year]
+clear all; help foxes;
 
-x1 = 300;
-y1 = 150;
-time = 0;
-tau = 1/365;
+a =2*12; %2[1/mo] --converted into 1/year
+b =1*12; %[1/mo] --Converted into 1/year
+c =0.12; %[1/year]
+d =0.06; %[1/year]
+
+x1 = 300; %Initial population of rabits
+y1 = 150; %initial population of foxes
+time = 0; %initial time
+tau = 1/365; %time step in units of 1/year
 
 xt = x1;
 yt = y1;
 state = [xt,yt]; %initial population
-param =[a, b, c, d];
-maxstep = 1000;
+param =[a, b, c, d]; %creating a parameter vector to pass to derivative function
+maxstep = 1000; %timesteps
 
-for iStep=1:maxstep
+for iStep=1:maxstep  %loop over interval to produce next value for plotting
 
 foxplot(iStep) = yt;
 rplot(iStep) = xt;	
@@ -32,59 +36,20 @@ tplot(iStep) = time;
 
 time = time + tau;
 
-state = rk4(state,time,tau,'deriveco',param);
+state = rk4(state,time,tau,'deriveco',param); %call to rk4 and derivavtiv function
 xt = state(1);
 yt = state(2);
 
 end
 figure(1)
 plot(tplot,rplot,'-',tplot,foxplot,'-');
-
+legend('Rabit population as a function of time', 'Fox population as a function of time');
+xlabel('time(years)')
 figure(2)
 plot(foxplot,rplot);
+legend('Rabit population as a function of Foxes');
+xlabel('Foxes');
+ylabel('Rabits');
+title('Prey as a function of preditors');
 
 
-
-% Euler's method for Lotka-Volterra equations (predator-prey system)
-% dR / dt = 0.08*R -   0.001 * W * R
-% dW / dt = -0.02 * W + 0.00002 * R * W
-% R=rabbits, W=wolves
-% Initial condition is R=300, W=40
-% Plots show populations as functions of time
-if 0
-t0=0;
-tn=1000;
-W(1)=150;
-R(1)=300;
-tau=1;
-
-t=t0:tau:1000;
-n=max(size(t));
-for i=2:n
-   rPrime=R(i-1)*(0.08-0.001*W(i-1));
-   wPrime=W(i-1)*(-0.02+0.00002*R(i-1));
-   W(i)=W(i-1)+tau*wPrime;
-   R(i)=R(i-1)+tau*rPrime;
-end
-figure(1)
-plot(t,R)
-title('Lotka-Volterra equations - rabbits population starting from R=1000')
-xlabel('time')
-ylabel('Rabbits')
-
-figure(2)
-plot(t,W)
-title('Lotka-Volterra equations - wolves population starting from W=40')
-xlabel('time')
-ylabel('Wolves')
-
-figure(3)
-plot(t,R,'r',t,W,'b')
-title('Lotka-Volterra equations - rabbits and wolves population starting from R=1000, W=40')
-xlabel('time')
-
-figure(4)
-plot(t,R/10,'r',t,W,'b')
-title('Lotka-Volterra equations - rabbits and wolves population starting from R=1000, W=40')
-xlabel('time')
-end
