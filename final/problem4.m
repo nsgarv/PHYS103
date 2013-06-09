b62c523fdec50d72d7a9fd06a9de79e0775a497d
Nathan Garvey <garvey.na@gmail.com>
2013-06-09 03:38:27 -0700

    problem 4 done

--- final/problem4.m
+++ final/problem4.m
@@ -1,6 +1,8 @@
 %% kirchoff's rules
 
 clear all;
+% as Rt -> inf current ig -> 3 amps
+% when Rt = 0; current ig = 8 amps
 
 r1 = 2/3;
 r2 = 10;
@@ -12,10 +14,13 @@ istep = length(rt);
 
 
 for i=1:istep
+	%% solved with equivelant resistance %%
 	req1 = ((2*r4*rt(i))/((2*r4)+rt(i)));
 	req2 = req1 + r3;
 	req3 = req2*r2/(req2 + r2);
 	rtot = req3 + 2*r1;
+
+	%% Matrix equations %%
 	%A = [(1/r1+1/r2+1/r3) -1/r3 -1/r2; 1/r3 (1/r3+1/rt(i)+1/(2*r4)) (-1/rt(i)-1/(2*r4)); 1/r1 0 1/r1];
 	%b = [-vg*1/r1; 0; -vg*1/r1];
 	%x = A\b;
@@ -28,18 +33,3 @@ ylabel('Current i (amps)');
 xlabel('Variable resistor Rt \Omega');
 title('current through power supply as a function of resistor Rt');
 
-%Ax = b
-if 0
-req1 = ((2*r4*rt(i))/((2*r4)+rt(i)));
-req2 = req1 + r3;
-req3 = req2*r2/(req2 + r2);
-rtot = req3 + 2*r1;
-
-
-	req1 = ((2*r4*rt(i))/((2*r4)+rt(i)));
-	num = req1 + r3 * r2;
-	denom = req1 +r3 + r2;
-	req(i) = num/denom + 2*r1;
-	ig(i) = vg/req(i);
-	%iplot(i) = i;
-end
